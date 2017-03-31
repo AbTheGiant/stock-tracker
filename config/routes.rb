8410def04c95dde780073ec12dc0dad0bca0c3c5
neymarsabin <reddevil.sabin@gmail.com>
2017-03-31 23:06:50 +0545

    stocks and users association and ability to add

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  resources :user_stocks, except: [:show, :edit,:update]
+
+  
   get 'static/welcome'
   root 'static#welcome'
   get 'static/index'
